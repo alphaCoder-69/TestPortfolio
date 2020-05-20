8d0acd173a2c0aeb72aafb32b852db5ef0e9dabe
alphaCoder-69 <eldrago2000@gmail.com>
2020-05-20 09:57:12 +0530

    Carousel

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/* Carousel Slider */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    animateOut:'fadeOut',
+    margin:80,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
